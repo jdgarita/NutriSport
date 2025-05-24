3e166deace0610955f40e2a4d735aab7da165660
JD <juandiegogarita@gmail.com>
2025-05-24 10:23:24 -0600

    Feat: Add auth module dependency to navigation
    This commit introduces a dependency on the `:feature:auth` module within the `:feature:navigation` module.
    
    The following line has been added to `feature/navigation/build.gradle.kts`:
    ```kotlin
    implementation(project(path = ":feature:auth"))
    ```

--- feature/navigation/build.gradle.kts
+++ feature/navigation/build.gradle.kts
@@ -41,6 +41,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(project(path = ":feature:auth"))
         }
     }
 }
