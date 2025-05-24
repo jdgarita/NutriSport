129214162d523259f9dc37ef4009d6b2f1d05964
JD <juandiegogarita@gmail.com>
2025-05-24 10:23:56 -0600

    Feat: Add initial navigation graph setup
    This commit introduces the `NavGraph.kt` file, which sets up the basic navigation structure for the application.
    
    Currently, it initializes the navigation with the `AuthScreen`.

---
+++ feature/navigation/src/commonMain/kotlin/dev/jdgarita/nutrisport/navigation/NavGraph.kt
@@ -0,0 +1,9 @@
+package dev.jdgarita.nutrisport.navigation
+
+import androidx.compose.runtime.Composable
+import dev.jdgarita.nutrisport.auth.AuthScreen
+
+@Composable
+fun SetupNavGraph() {
+    AuthScreen()
+}
