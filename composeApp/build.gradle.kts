5975a87564d72116ae47ec7cee252a4970ec2729
JD <juandiegogarita@gmail.com>
2025-05-24 10:23:45 -0600

    Feat: Integrate navigation module into Android app
    The `navigation` feature module has been added as a dependency to the `androidMain` source set in `composeApp/build.gradle.kts`. This will allow the Android application to utilize the navigation components defined in the `:feature:navigation` module.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(project(path = ":feature:navigation"))
         }
     }
 }
