29f96887656e9e3decfd5620799c412f6b09dcde
JD <juandiegogarita@gmail.com>
2025-05-24 09:24:59 -0600

    Feat: Implement AuthScreen UI
    This commit introduces the initial UI for the authentication screen (`AuthScreen.kt`).
    
    The screen includes:
    - A title "NUTRISPORT" and a subtitle "Sign in to continue".
    - A Google Sign-In button (`GoogleButton`).
    - Integration with `ContentWithMessageBar` for displaying messages.
    
    The UI elements are styled using predefined font sizes, colors, and alpha values from the `shared` module.

---
+++ feature/auth/src/commonMain/kotlin/dev/jdgarita/nutrisport/auth/AuthScreen.kt
@@ -0,0 +1,67 @@
+package dev.jdgarita.nutrisport.auth
+
+import ContentWithMessageBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
+import dev.jdgarita.nutrisport.auth.component.GoogleButton
+import dev.jdgarita.nutrisport.shared.Alpha
+import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.TextSecondary
+import rememberMessageBarState
+
+@Composable
+fun AuthScreen() {
+    val messageBarState = rememberMessageBarState()
+    Scaffold { padding ->
+        ContentWithMessageBar(
+            modifier = Modifier.padding(
+                top = padding.calculateTopPadding(),
+                bottom = padding.calculateBottomPadding()
+            ),
+            messageBarState = messageBarState,
+            errorMaxLines = 2,
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "NUTRISPORT",
+                        textAlign = TextAlign.Center,
+                        fontFamily = BebasNeueFont(),
+                        fontSize = FontSize.EXTRA_LARGE,
+                        color = TextSecondary
+                    )
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .alpha(Alpha.HALF),
+                        text = "Sign in to continue",
+                        textAlign = TextAlign.Center,
+                        fontSize = FontSize.EXTRA_REGULAR,
+                        color = TextPrimary
+                    )
+                }
+                GoogleButton(
+                    loading = false,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
