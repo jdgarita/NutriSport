c5adb7e786ff6e7c8deeddde6288753e2c020a41
JD <juandiegogarita@gmail.com>
2025-05-24 10:23:34 -0600

    Refactor: Apply padding and background color to AuthScreen
    This commit updates `AuthScreen.kt` by:
    - Applying a `Surface` background color to `ContentWithMessageBar`.
    - Adding a 24.dp padding to the main `Column` within the screen.

--- feature/auth/src/commonMain/kotlin/dev/jdgarita/nutrisport/auth/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/dev/jdgarita/nutrisport/auth/AuthScreen.kt
@@ -13,10 +13,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import dev.jdgarita.nutrisport.auth.component.GoogleButton
 import dev.jdgarita.nutrisport.shared.Alpha
 import dev.jdgarita.nutrisport.shared.BebasNeueFont
 import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.TextPrimary
 import dev.jdgarita.nutrisport.shared.TextSecondary
 import rememberMessageBarState
@@ -26,6 +28,7 @@ fun AuthScreen() {
     val messageBarState = rememberMessageBarState()
     Scaffold { padding ->
         ContentWithMessageBar(
+            contentBackgroundColor = Surface,
             modifier = Modifier.padding(
                 top = padding.calculateTopPadding(),
                 bottom = padding.calculateBottomPadding()
@@ -33,7 +36,11 @@ fun AuthScreen() {
             messageBarState = messageBarState,
             errorMaxLines = 2,
         ) {
-            Column(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = 24.dp)
+            ) {
                 Column(
                     modifier = Modifier.weight(1f),
                     verticalArrangement = Arrangement.Center,
