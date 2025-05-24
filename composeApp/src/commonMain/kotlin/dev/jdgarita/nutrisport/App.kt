5795bf408e2226ce0190737e217b07b1b1bd7eb6
JD <juandiegogarita@gmail.com>
2025-05-24 10:24:15 -0600

    Feat: Integrate navigation graph into App
    The `SetupNavGraph` composable has been added to the `App` composable, enabling navigation within the application.

--- composeApp/src/commonMain/kotlin/dev/jdgarita/nutrisport/App.kt
+++ composeApp/src/commonMain/kotlin/dev/jdgarita/nutrisport/App.kt
@@ -2,11 +2,13 @@ package dev.jdgarita.nutrisport
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import dev.jdgarita.nutrisport.navigation.SetupNavGraph
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        SetupNavGraph()
     }
 }
