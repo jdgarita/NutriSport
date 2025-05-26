a9cd39c773943b8befd54e0ec5e0ee42f17ffef5
JD <juandiegogarita@gmail.com>
2025-05-26 09:52:39 -0600

    Feat: Initialize Firebase on Android application start
    This commit introduces `MyApplication.kt`, a custom `Application` class.
    The `Firebase.initialize(this)` method is called within the `onCreate()` lifecycle method of this class to ensure Firebase is initialized when the Android application starts.

---
+++ composeApp/src/androidMain/kotlin/dev/jdgarita/nutrisport/MyApplication.kt
@@ -0,0 +1,14 @@
+package dev.jdgarita.nutrisport
+
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Firebase.initialize(this)
+    }
+}
