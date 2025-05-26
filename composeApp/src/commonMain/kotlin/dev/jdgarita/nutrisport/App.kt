5edf50e7a05aa8ec9aa3d0f9f432a288e020fd52
JD <juandiegogarita@gmail.com>
2025-05-26 09:53:13 -0600

    Feat: Initialize GoogleAuthProvider on app launch
    This commit introduces the initialization of `GoogleAuthProvider` when the app starts.
    
    - `GoogleAuthProvider.create()` is called within a `LaunchedEffect` block.
    - A `GoogleAuthCredentials` object with the `WEB_CLIENT_ID` is passed to the provider.
    - The main navigation graph (`SetupNavGraph`) is now displayed within an `AnimatedVisibility` block, which becomes visible only after `GoogleAuthProvider` has been initialized and `appReady` is true.

--- composeApp/src/commonMain/kotlin/dev/jdgarita/nutrisport/App.kt
+++ composeApp/src/commonMain/kotlin/dev/jdgarita/nutrisport/App.kt
@@ -1,14 +1,35 @@
 package dev.jdgarita.nutrisport
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.mmk.kmpauth.google.GoogleAuthCredentials
+import com.mmk.kmpauth.google.GoogleAuthProvider
 import dev.jdgarita.nutrisport.navigation.SetupNavGraph
+import dev.jdgarita.nutrisport.shared.Constants.WEB_CLIENT_ID
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        SetupNavGraph()
+        var appReady by remember { mutableStateOf(false) }
+        LaunchedEffect(Unit) {
+            GoogleAuthProvider.create(credentials = GoogleAuthCredentials(serverId = WEB_CLIENT_ID))
+            appReady = true
+        }
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxSize(),
+            visible = appReady
+        ) {
+            SetupNavGraph()
+        }
     }
 }
