f910b6ddff0378c48c25aa7183dd4ff5c7f6642f
JD <juandiegogarita@gmail.com>
2025-05-26 12:16:22 -0600

    includes auth logic for iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,31 @@
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(
+                perform: { url in
+                            GIDSignIn.sharedInstance.handle(url)
+                        }
+            )
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        open url: URL,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        var handled: Bool
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        
+        return false
     }
 }
