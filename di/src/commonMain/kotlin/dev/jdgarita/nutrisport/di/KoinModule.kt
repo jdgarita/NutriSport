930229688774b4e6443c38c05e9959d3791aba76
JD <juandiegogarita@gmail.com>
2025-05-27 21:40:38 -0600

    Feat: Add customer creation functionality
    This commit introduces the `CustomerRepository` interface and its implementation `CustomerRepositoryImpl` to handle the creation of customer documents in Firestore.
    
    The `createCustomer` function:
    - Takes a `FirebaseUser` object.
    - Extracts user information (UID, display name, email) to populate a `Customer` object.
    - Sets default values for missing information and other customer fields (city, postalCode, address, phoneNumber, cart, isAdmin).
    - Checks if a customer document with the user's UID already exists in the "customer" collection.
    - If the customer does not exist, it creates a new document with the user's UID.
    - Calls `onSuccess` if the operation is successful (either the customer already exists or is newly created).
    - Calls `onError` with an appropriate message if the user is null or if any exception occurs during the process.

--- di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
+++ di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
@@ -1,12 +1,23 @@
 package dev.jdgarita.nutrisport.di
 
+import dev.jdgarita.nutrisport.auth.AuthViewModel
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+import dev.jdgarita.nutrisport.data.domain.CustomerRepository
+import dev.jdgarita.nutrisport.data.CustomerRepositoryImpl
+
+val sharedModule = module {
+    single<CustomerRepository> { CustomerRepositoryImpl() }
+    viewModelOf(::AuthViewModel)
+}
 
 fun initializeKoin(
     config: (KoinApplication.() -> Unit)? = null
 ) {
     startKoin {
         config?.invoke(this)
+        modules(sharedModule)
     }
 }
