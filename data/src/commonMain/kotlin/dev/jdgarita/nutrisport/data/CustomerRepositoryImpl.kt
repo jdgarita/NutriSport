74e6d2d79b68fb791f78ba4faafbd9bc141b3abf
JD <juandiegogarita@gmail.com>
2025-05-27 21:40:30 -0600

    Feat: Add customer creation functionality
    This commit introduces the `CustomerRepository` interface and its implementation `CustomerRepositoryImpl` to handle the creation of customer documents in Firestore.
    
    The `createCustomer` function:
    - Takes a `FirebaseUser` object.
    - Extracts user information (UID, display name, email) to populate a `Customer` object.
    - Sets default values for missing information and other customer fields (city, postalCode, address, phoneNumber, cart, isAdmin).
    - Checks if a customer document with the user's UID already exists in the "customer" collection.
    - If the customer does not exist, it creates a new document with the user's UID.
    - Calls `onSuccess` if the operation is successful (either the customer already exists or is newly created).
    - Calls `onError` with an appropriate message if the user is null or if any exception occurs during the process.

---
+++ data/src/commonMain/kotlin/dev/jdgarita/nutrisport/data/CustomerRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package dev.jdgarita.nutrisport.data
+
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.FirebaseUser
+import dev.gitlive.firebase.firestore.firestore
+import dev.jdgarita.nutrisport.data.domain.CustomerRepository
+import dev.jdgarita.nutrisport.shared.domain.Customer
+
+class CustomerRepositoryImpl : CustomerRepository {
+
+    override suspend fun createCustomer(
+        user: FirebaseUser?,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) = try {
+        if (user != null) {
+            val customerCollection = Firebase.firestore.collection(collectionPath = "customer")
+            val customer = Customer(
+                id = user.uid,
+                firstName = user.displayName?.split(" ")?.firstOrNull() ?: "Unknown",
+                lastName = user.displayName?.split(" ")?.lastOrNull() ?: "Unknown",
+                email = user.email ?: "Unknown",
+                city = null,
+                postalCode = null,
+                address = null,
+                phoneNumber = null,
+                cart = emptyList(),
+                isAdmin = false
+            )
+
+            val customerExists = customerCollection.document(user.uid).get().exists
+
+            when {
+                customerExists -> onSuccess()
+                else -> {
+                    customerCollection.document(user.uid).set(customer)
+                    onSuccess()
+                }
+            }
+
+        } else {
+            onError("User is not available.")
+        }
+    } catch (e: Exception) {
+        onError("Error while creating a Customer: ${e.message ?: "Unknown error"}")
+    }
+}
---
+++ data/src/commonMain/kotlin/dev/jdgarita/nutrisport/data/domain/CustomerRepository.kt
@@ -0,0 +1,12 @@
+package dev.jdgarita.nutrisport.data.domain
+
+import dev.gitlive.firebase.auth.FirebaseUser
+
+interface CustomerRepository {
+
+    suspend fun createCustomer(
+        user: FirebaseUser?,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    )
+}
