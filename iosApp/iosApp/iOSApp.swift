85e0130f309b54b1fc1b2e1c1eabe6223613125f
JD <juandiegogarita@gmail.com>
2025-05-27 20:13:01 -0600

    Update iOSApp.swift

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 import GoogleSignIn
+import Firebase
 
 @main
 struct iOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView().onOpenURL(
@@ -15,10 +19,19 @@ struct iOSApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
     func application(
         _ application: UIApplication,
         open url: URL,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
     ) -> Bool {
         var handled: Bool
         handled = GIDSignIn.sharedInstance.handle(url)
