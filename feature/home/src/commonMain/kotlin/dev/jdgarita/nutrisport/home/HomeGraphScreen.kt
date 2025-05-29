b007a4bdf588ca6aa8d882c810478b76e6ef1ef7
JD <juandiegogarita@gmail.com>
2025-05-28 18:10:06 -0600

    Feat: Implement HomeGraphScreen layout with Scaffold and BottomBar

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
@@ -1,7 +1,38 @@
 package dev.jdgarita.nutrisport.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.jdgarita.nutrisport.home.component.BottomBar
+import dev.jdgarita.nutrisport.shared.Surface
 
 @Composable
 fun HomeGraphScreen() {
+
+    Scaffold(
+        containerColor = Surface
+    ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(
+                top = padding.calculateTopPadding(),
+                bottom = padding.calculateBottomPadding()
+            )
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Box(
+                modifier = Modifier.padding(all = 12.dp)
+            ) {
+                BottomBar(
+                    selected = false,
+                    onSelect = { /* Handle selection */ }
+                )
+            }
+        }
+    }
 }
