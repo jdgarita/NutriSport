7afe1e9efb07b853c6a869feb49c700c357ed322
JD <juandiegogarita@gmail.com>
2025-05-31 10:49:28 -0600

    Refactor: Remove unused imports from ProfileScreen.kt

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -1,14 +1,6 @@
 package dev.jdgarita.nutrisport.profile
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import dev.jdgarita.nutrisport.shared.Resources
-import dev.jdgarita.nutrisport.shared.component.PrimaryButton
 
 @Composable
 fun ProfileScreen() {
