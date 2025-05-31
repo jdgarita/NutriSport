98c3741dde219f7bb858a5fc90737a56c689843a
JD <juandiegogarita@gmail.com>
2025-05-31 13:33:46 -0600

    Feat: Enhance ProfileScreen layout with background and padding adjustments

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -1,25 +1,35 @@
 package dev.jdgarita.nutrisport.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.component.ProfileForm
 
 @Composable
 fun ProfileScreen() {
-    ProfileForm(
-        modifier = Modifier,
-        firstName = "",
-        onFirstNameChange = {},
-        lastName = "",
-        onLastNameChange = {},
-        email = "",
-        city = "",
-        onCityChange = {},
-        postalCode = null,
-        onPostalCodeChange = {},
-        address = "",
-        onAddressChange = {},
-        phoneNumber = null,
-        onPhoneNumberChange = {}
-    )
+    Box(
+        modifier = Modifier
+            .background(Surface)
+            .systemBarsPadding()
+    ) {
+        ProfileForm(
+            modifier = Modifier,
+            firstName = "Juan Diego",
+            onFirstNameChange = {},
+            lastName = "Garita Segura",
+            onLastNameChange = {},
+            email = "",
+            city = "",
+            onCityChange = {},
+            postalCode = null,
+            onPostalCodeChange = {},
+            address = "",
+            onAddressChange = {},
+            phoneNumber = null,
+            onPhoneNumberChange = {}
+        )
+    }
 }
