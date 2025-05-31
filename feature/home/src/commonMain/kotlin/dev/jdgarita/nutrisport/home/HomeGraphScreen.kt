48d1c23141d88ed6f36bae79925978932779af1b
JD <juandiegogarita@gmail.com>
2025-05-30 18:46:51 -0600

    Feat: Implement navigation for HomeGraphScreen with NavHost and composable destinations

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dev.jdgarita.nutrisport.home.component.BottomBar
 import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.navigation.Screen
 
 @Composable
 fun HomeGraphScreen() {
+    val navigationController = rememberNavController()
 
     Scaffold(
         containerColor = Surface
@@ -24,7 +30,16 @@ fun HomeGraphScreen() {
                 bottom = padding.calculateBottomPadding()
             )
         ) {
-            Spacer(modifier = Modifier.weight(1f))
+            NavHost(
+                modifier = Modifier.weight(1f),
+                navController = navigationController,
+                startDestination = Screen.ProductOverview
+            ) {
+                composable<Screen.ProductOverview> {}
+                composable<Screen.Cart> {}
+                composable<Screen.Categories> {}
+            }
+            Spacer(modifier = Modifier.height(12.dp))
             Box(
                 modifier = Modifier.padding(all = 12.dp)
             ) {
