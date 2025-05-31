6a48456d829ad3f8db9b7467ee809f44a8273fd9
JD <juandiegogarita@gmail.com>
2025-05-30 19:01:30 -0600

    Feat: Refactor BottomBar selection state management using derivedStateOf

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dev.jdgarita.nutrisport.home.component.BottomBar
 import dev.jdgarita.nutrisport.home.domain.BottomBarDestination
@@ -25,8 +27,19 @@ import dev.jdgarita.nutrisport.shared.navigation.Screen
 @Composable
 fun HomeGraphScreen() {
     val navigationController = rememberNavController()
-    var selectedDestination by remember { mutableStateOf(BottomBarDestination.ProductOverView) }
+    val currentRoute = navigationController.currentBackStackEntryAsState()
 
+    val selectedDestination by remember {
+        derivedStateOf {
+            val route = currentRoute.value?.destination?.route.toString()
+            when {
+                route.contains(BottomBarDestination.ProductOverView.screen.toString()) -> BottomBarDestination.ProductOverView
+                route.contains(BottomBarDestination.Cart.screen.toString()) -> BottomBarDestination.Cart
+                route.contains(BottomBarDestination.Categories.screen.toString()) -> BottomBarDestination.Categories
+                else -> BottomBarDestination.ProductOverView
+            }
+        }
+    }
     Scaffold(
         containerColor = Surface
     ) { padding ->
@@ -52,7 +65,6 @@ fun HomeGraphScreen() {
                 BottomBar(
                     selected = selectedDestination,
                     onSelect = { destination ->
-                        selectedDestination = destination
                         navigationController.navigate(destination.screen) {
                             launchSingleTop = true
                             popUpTo(Screen.ProductOverview) {
