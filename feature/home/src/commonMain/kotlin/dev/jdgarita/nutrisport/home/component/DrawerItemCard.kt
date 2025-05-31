31205304473452043b544c287cffd730f452f19a
JD <juandiegogarita@gmail.com>
2025-05-30 19:57:13 -0600

    Feat: Update DrawerItemCard to fill width and add rounded corners

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/DrawerItemCard.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/DrawerItemCard.kt
@@ -3,13 +3,16 @@ package dev.jdgarita.nutrisport.home.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import dev.jdgarita.nutrisport.home.domain.DrawerItem
 import dev.jdgarita.nutrisport.shared.FontSize
@@ -23,6 +26,8 @@ fun DrawerItemCard(
 ) {
     Row(
         modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(99.dp))
             .padding(vertical = 12.dp, horizontal = 12.dp)
             .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
