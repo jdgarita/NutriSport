a14d4611d669d001c78347ec523a143489419fad
JD <juandiegogarita@gmail.com>
2025-05-30 22:05:25 -0600

    Feat: Add HomeGraphViewModel to Koin module for dependency injection

--- di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
+++ di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
@@ -1,16 +1,18 @@
 package dev.jdgarita.nutrisport.di
 
 import dev.jdgarita.nutrisport.auth.AuthViewModel
+import dev.jdgarita.nutrisport.data.CustomerRepositoryImpl
+import dev.jdgarita.nutrisport.data.domain.CustomerRepository
+import dev.jdgarita.nutrisport.home.HomeGraphViewModel
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-import dev.jdgarita.nutrisport.data.domain.CustomerRepository
-import dev.jdgarita.nutrisport.data.CustomerRepositoryImpl
 
 val sharedModule = module {
     single<CustomerRepository> { CustomerRepositoryImpl() }
     viewModelOf(::AuthViewModel)
+    viewModelOf(::HomeGraphViewModel)
 }
 
 fun initializeKoin(
