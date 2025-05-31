34d05e3afe0e7a0c2dc14e5215daeba69f6a2617
JD <juandiegogarita@gmail.com>
2025-05-31 13:23:04 -0600

    Feat: Integrate ProfileForm into ProfileScreen with initial state handling

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -1,7 +1,25 @@
 package dev.jdgarita.nutrisport.profile
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.jdgarita.nutrisport.shared.component.ProfileForm
 
 @Composable
 fun ProfileScreen() {
+    ProfileForm(
+        modifier = Modifier,
+        firstName = "",
+        onFirstNameChange = {},
+        lastName = "",
+        onLastNameChange = {},
+        email = "",
+        city = "",
+        onCityChange = {},
+        postalCode = null,
+        onPostalCodeChange = {},
+        address = "",
+        onAddressChange = {},
+        phoneNumber = null,
+        onPhoneNumberChange = {}
+    )
 }
