6ee3e7735af5380d4b18af54cb77fcf3927da864
JD <juandiegogarita@gmail.com>
2025-05-30 19:49:07 -0600

    Feat: Add DrawerItemCard composable for rendering navigation drawer items

---
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/DrawerItemCard.kt
@@ -0,0 +1,42 @@
+package dev.jdgarita.nutrisport.home.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.jdgarita.nutrisport.home.domain.DrawerItem
+import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.IconPrimary
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun DrawerItemCard(
+    drawerItem: DrawerItem,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(vertical = 12.dp, horizontal = 12.dp)
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(drawerItem.icon),
+            contentDescription = "Drawer item icon",
+            tint = IconPrimary
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = drawerItem.title,
+            color = IconPrimary,
+            fontSize = FontSize.EXTRA_REGULAR
+        )
+    }
+}
