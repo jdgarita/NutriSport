c6d7ec79cd1d8babeeba1b0afc53a71bffbefdfd
JD <juandiegogarita@gmail.com>
2025-05-30 21:50:44 -0600

    Refactor: Remove unused imports in CustomDrawer.kt

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/CustomDrawer.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/CustomDrawer.kt
@@ -1,6 +1,5 @@
 package dev.jdgarita.nutrisport.home.component
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
