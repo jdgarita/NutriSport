7f60ab00569717d4884d8d1a30d9ea167ddbf65e
JD <juandiegogarita@gmail.com>
2025-05-30 18:52:00 -0600

    Feat: Implement state management for BottomBar selection in HomeGraphScreen

--- feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/HomeGraphScreen.kt
@@ -8,18 +8,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.jdgarita.nutrisport.home.component.BottomBar
+import dev.jdgarita.nutrisport.home.domain.BottomBarDestination
 import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.navigation.Screen
 
 @Composable
 fun HomeGraphScreen() {
     val navigationController = rememberNavController()
+    var selectedDestination by remember { mutableStateOf(BottomBarDestination.ProductOverView) }
 
     Scaffold(
         containerColor = Surface
@@ -44,8 +50,9 @@ fun HomeGraphScreen() {
                 modifier = Modifier.padding(all = 12.dp)
             ) {
                 BottomBar(
-                    selected = false,
+                    selected = selectedDestination,
                     onSelect = { destination ->
+                        selectedDestination = destination
                         navigationController.navigate(destination.screen) {
                             launchSingleTop = true
                             popUpTo(Screen.ProductOverview) {
