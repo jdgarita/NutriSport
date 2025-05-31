7842e42c47ad87baaf2b111019e037bb0c0ab112
JD <juandiegogarita@gmail.com>
2025-05-30 19:49:12 -0600

    Feat: Add CustomDrawer composable for navigation drawer layout and item handling

---
+++ feature/home/src/commonMain/kotlin/dev/jdgarita/nutrisport/home/component/CustomDrawer.kt
@@ -0,0 +1,75 @@
+package dev.jdgarita.nutrisport.home.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import dev.jdgarita.nutrisport.home.domain.DrawerItem
+import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.TextSecondary
+
+@Composable
+fun CustomDrawer(
+    onProfileClick: () -> Unit,
+    onContactUsClick: () -> Unit,
+    onSignOutClick: () -> Unit,
+    onAdminPanelClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(fraction = 0.60f)
+            .fillMaxHeight()
+            .padding(horizontal = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "NutriSport",
+            textAlign = TextAlign.Center,
+            color = TextSecondary,
+            fontFamily = BebasNeueFont(),
+            fontSize = FontSize.EXTRA_LARGE
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Healthy lifestyle",
+            textAlign = TextAlign.Center,
+            color = TextPrimary,
+            fontSize = FontSize.REGULAR
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        DrawerItem.entries.take(5).forEach { drawerItem ->
+            DrawerItemCard(
+                drawerItem = drawerItem,
+                onClick = {
+                    when (drawerItem) {
+                        DrawerItem.Profile -> onProfileClick()
+                        DrawerItem.Contact -> onContactUsClick()
+                        DrawerItem.SignOut -> onSignOutClick()
+                        else -> {}
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        DrawerItemCard(
+            drawerItem = DrawerItem.AdminPanel,
+            onClick = onAdminPanelClick
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
