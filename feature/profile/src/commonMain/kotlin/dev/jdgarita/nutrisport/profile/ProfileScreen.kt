22fc3213ac0a0646d8a9373597e69f060bdbac0e
JD <juandiegogarita@gmail.com>
2025-05-31 15:39:26 -0600

    Feat: Add country selection state management in ProfileScreen

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -4,12 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.component.ProfileForm
+import dev.jdgarita.nutrisport.shared.domain.Country
 
 @Composable
 fun ProfileScreen() {
+    var country by remember { mutableStateOf(Country.Serbia) }
     Box(
         modifier = Modifier
             .background(Surface)
@@ -29,7 +35,11 @@ fun ProfileScreen() {
             address = "",
             onAddressChange = {},
             phoneNumber = null,
-            onPhoneNumberChange = {}
+            onPhoneNumberChange = {},
+            country = country,
+            onCountrySelect = { selectedCountry ->
+                country = selectedCountry
+            }
         )
     }
 }
