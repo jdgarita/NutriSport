82803637b70e7bcf7d30c4fba1ad1e1cdd76ac26
JD <juandiegogarita@gmail.com>
2025-06-01 22:03:01 -0600

    Feat: Register ProfileViewModel in Koin module for dependency injection

--- di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
+++ di/src/commonMain/kotlin/dev/jdgarita/nutrisport/di/KoinModule.kt
@@ -4,6 +4,7 @@ import dev.jdgarita.nutrisport.auth.AuthViewModel
 import dev.jdgarita.nutrisport.data.CustomerRepositoryImpl
 import dev.jdgarita.nutrisport.data.domain.CustomerRepository
 import dev.jdgarita.nutrisport.home.HomeGraphViewModel
+import dev.jdgarita.nutrisport.profile.ProfileViewModel
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.viewModelOf
@@ -13,6 +14,7 @@ val sharedModule = module {
     single<CustomerRepository> { CustomerRepositoryImpl() }
     viewModelOf(::AuthViewModel)
     viewModelOf(::HomeGraphViewModel)
+    viewModelOf(::ProfileViewModel)
 }
 
 fun initializeKoin(
