e94b834700ddb3d2f4f81e4962d19420d8dac4cc
JD <juandiegogarita@gmail.com>
2025-06-01 21:39:38 -0600

    Feat: Refactor ProfileScreen layout to include padding and update button

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -1,5 +1,9 @@
 package dev.jdgarita.nutrisport.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,12 +16,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import dev.jdgarita.nutrisport.shared.BebasNeueFont
 import dev.jdgarita.nutrisport.shared.FontSize
 import dev.jdgarita.nutrisport.shared.IconPrimary
 import dev.jdgarita.nutrisport.shared.Resources
 import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.component.PrimaryButton
 import dev.jdgarita.nutrisport.shared.component.ProfileForm
 import dev.jdgarita.nutrisport.shared.domain.Country
 import org.jetbrains.compose.resources.painterResource
@@ -54,26 +60,40 @@ fun ProfileScreen(
                 }
             )
         })
-    {
-        ProfileForm(
-            modifier = Modifier,
-            firstName = "Juan Diego",
-            onFirstNameChange = {},
-            lastName = "Garita Segura",
-            onLastNameChange = {},
-            email = "",
-            city = "",
-            onCityChange = {},
-            postalCode = null,
-            onPostalCodeChange = {},
-            address = "",
-            onAddressChange = {},
-            phoneNumber = null,
-            onPhoneNumberChange = {},
-            country = country,
-            onCountrySelect = { selectedCountry ->
-                country = selectedCountry
+    { padding ->
+        Column(
+            modifier = Modifier.padding(
+                top = padding.calculateTopPadding(),
+                bottom = padding.calculateBottomPadding()
+            )
+        ) {
+            ProfileForm(
+                modifier = Modifier.weight(1f),
+                firstName = "Juan Diego",
+                onFirstNameChange = {},
+                lastName = "Garita Segura",
+                onLastNameChange = {},
+                email = "",
+                city = "",
+                onCityChange = {},
+                postalCode = null,
+                onPostalCodeChange = {},
+                address = "",
+                onAddressChange = {},
+                phoneNumber = null,
+                onPhoneNumberChange = {},
+                country = country,
+                onCountrySelect = { selectedCountry ->
+                    country = selectedCountry
+                }
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            PrimaryButton(
+                text = "Update",
+                icon = Resources.Icon.Checkmark,
+            ) {
+
             }
-        )
+        }
     }
 }
