f4ebbfd8c43894e280c698f722fd8c0574fb95a6
JD <juandiegogarita@gmail.com>
2025-06-01 21:37:07 -0600

    Feat: Enhance ProfileScreen with navigation and top app bar

--- feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/dev/jdgarita/nutrisport/profile/ProfileScreen.kt
@@ -1,26 +1,60 @@
 package dev.jdgarita.nutrisport.profile
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.IconPrimary
+import dev.jdgarita.nutrisport.shared.Resources
 import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.TextPrimary
 import dev.jdgarita.nutrisport.shared.component.ProfileForm
 import dev.jdgarita.nutrisport.shared.domain.Country
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(
+    navigateBack: () -> Unit,
+) {
     var country by remember { mutableStateOf(Country.Serbia) }
-    Box(
-        modifier = Modifier
-            .background(Surface)
-            .systemBarsPadding()
-    ) {
+
+    Scaffold(
+        containerColor = Surface,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "My Profile",
+                        fontFamily = BebasNeueFont(),
+                        fontSize = FontSize.LARGE,
+                        color = TextPrimary
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = navigateBack
+                    ) {
+                        Icon(
+                            painter = painterResource(Resources.Icon.BackArrow),
+                            contentDescription = "Back arrow icon",
+                            tint = IconPrimary
+                        )
+                    }
+                }
+            )
+        })
+    {
         ProfileForm(
             modifier = Modifier,
             firstName = "Juan Diego",
