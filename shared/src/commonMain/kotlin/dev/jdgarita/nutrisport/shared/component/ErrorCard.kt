f06e02913617f3d2cf15a4dee9c83eb113e59dfe
JD <juandiegogarita@gmail.com>
2025-06-02 19:06:49 -0600

    Feat: Enhance ErrorCard to support customizable font size

--- shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/component/ErrorCard.kt
+++ shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/component/ErrorCard.kt
@@ -7,12 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import dev.jdgarita.nutrisport.shared.FontSize
 
 @Composable
 fun ErrorCard(
     modifier: Modifier = Modifier,
-    message: String
+    message: String,
+    fontSize: TextUnit = FontSize.SMALL
 ) {
     Box(
         modifier = modifier,
@@ -22,7 +24,7 @@ fun ErrorCard(
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
             text = message,
-            fontSize = FontSize.SMALL
+            fontSize = fontSize
         )
     }
 }
