93283336309c6ef1188ccb94b0631e6fb8dd0421
JD <juandiegogarita@gmail.com>
2025-06-04 21:07:16 -0600

    Implement top app bar in ManageProductScreen with navigation and title handling

--- feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
+++ feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
@@ -1,9 +1,64 @@
 package dev.jdgarita.nutrisport.manage_product
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.FontSize
+import dev.jdgarita.nutrisport.shared.IconPrimary
+import dev.jdgarita.nutrisport.shared.Resources
+import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.TextPrimary
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ManageProductScreen(
+    id: String?,
     navigateBack: () -> Unit
 ) {
+    Scaffold(
+        containerColor = Surface,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = if (id == null) "New Product"
+                        else "Edit Product",
+                        fontFamily = BebasNeueFont(),
+                        fontSize = FontSize.LARGE,
+                        color = TextPrimary
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = navigateBack) {
+                        Icon(
+                            painter = painterResource(Resources.Icon.BackArrow),
+                            contentDescription = "Back arrow icon",
+                            tint = IconPrimary
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Surface,
+                    scrolledContainerColor = Surface,
+                    navigationIconContentColor = IconPrimary,
+                    titleContentColor = TextPrimary,
+                    actionIconContentColor = IconPrimary
+                )
+            )
+        }
+    )
+    { padding ->
+
+        Column {
+
+        }
+    }
 }
