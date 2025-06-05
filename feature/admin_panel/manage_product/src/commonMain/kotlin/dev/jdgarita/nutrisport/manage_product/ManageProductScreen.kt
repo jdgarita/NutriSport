0d31f033448e1feb229d4992f0989489490271fb
JD <juandiegogarita@gmail.com>
2025-06-04 22:05:40 -0600

    Add a decorative box with an icon to ManageProductScreen for enhanced UI

--- feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
+++ feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
@@ -1,14 +1,19 @@
 package dev.jdgarita.nutrisport.manage_product
 
 import ContentWithMessageBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,14 +24,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.BorderIdle
 import dev.jdgarita.nutrisport.shared.FontSize
 import dev.jdgarita.nutrisport.shared.IconPrimary
 import dev.jdgarita.nutrisport.shared.Resources
 import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.SurfaceLighter
 import dev.jdgarita.nutrisport.shared.TextPrimary
 import dev.jdgarita.nutrisport.shared.component.AlertTextField
 import dev.jdgarita.nutrisport.shared.component.CustomTextField
@@ -96,6 +105,28 @@ fun ManageProductScreen(
                         .verticalScroll(rememberScrollState()),
                     verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .border(
+                                width = 1.dp,
+                                color = BorderIdle,
+                                shape = RoundedCornerShape(12.dp)
+                            )
+                            .background(SurfaceLighter),
+                        contentAlignment = Alignment.Center
+                    )
+                    {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(Resources.Icon.Plus),
+                            contentDescription = "Plus icon",
+                            tint = IconPrimary
+                        )
+                    }
                     CustomTextField(
                         value = "",
                         onValueChange = { /* Handle value change */ },
