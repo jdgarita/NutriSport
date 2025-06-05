897bea7d0dff7faabcf3f1d98921dea2ac31280a
JD <juandiegogarita@gmail.com>
2025-06-04 21:19:34 -0600

    Add content layout and input fields to ManageProductScreen

--- feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
+++ feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductScreen.kt
@@ -1,6 +1,14 @@
 package dev.jdgarita.nutrisport.manage_product
 
+import ContentWithMessageBar
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -9,13 +17,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import dev.jdgarita.nutrisport.shared.BebasNeueFont
 import dev.jdgarita.nutrisport.shared.FontSize
 import dev.jdgarita.nutrisport.shared.IconPrimary
 import dev.jdgarita.nutrisport.shared.Resources
 import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.component.AlertTextField
+import dev.jdgarita.nutrisport.shared.component.CustomTextField
+import dev.jdgarita.nutrisport.shared.component.PrimaryButton
 import org.jetbrains.compose.resources.painterResource
+import rememberMessageBarState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,6 +38,8 @@ fun ManageProductScreen(
     id: String?,
     navigateBack: () -> Unit
 ) {
+    val messageBarState = rememberMessageBarState()
+
     Scaffold(
         containerColor = Surface,
         topBar = {
@@ -57,8 +74,73 @@ fun ManageProductScreen(
     )
     { padding ->
 
-        Column {
+        ContentWithMessageBar(
+            modifier = Modifier.fillMaxSize().padding(
+                top = padding.calculateTopPadding(),
+                bottom = padding.calculateBottomPadding()
+            ),
+            messageBarState = messageBarState,
+            errorMaxLines = 2,
+            contentBackgroundColor = Surface
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 24.dp)
+                    .padding(bottom = 24.dp)
+                    .padding(top = 12.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    CustomTextField(
+                        value = "",
+                        onValueChange = { /* Handle value change */ },
+                        placeholder = "Title",
+                    )
+                    CustomTextField(
+                        modifier = Modifier.height(168.dp),
+                        value = "",
+                        onValueChange = { /* Handle value change */ },
+                        placeholder = "Description",
+                        expanded = true
+                    )
+                    AlertTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Protein",
+                        onClick = {}
+                    )
+                    CustomTextField(
+                        value = "",
+                        onValueChange = { /* Handle value change */ },
+                        placeholder = "Weight (Optional",
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+                    CustomTextField(
+                        value = "",
+                        onValueChange = { /* Handle value change */ },
+                        placeholder = "Flavors (Optional)",
+                    )
+                    CustomTextField(
+                        value = "",
+                        onValueChange = { /* Handle value change */ },
+                        placeholder = "price",
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
 
+                    Spacer(modifier = Modifier.height(24.dp))
+                }
+
+                PrimaryButton(
+                    text = if (id == null) "Add new product" else "Update",
+                    icon = if (id == null) Resources.Icon.Plus else Resources.Icon.Checkmark,
+                    onClick = {}
+                )
+            }
         }
     }
 }
