6bc02242d4a0dfa498af90db1eef814ac4a9e996
JD <juandiegogarita@gmail.com>
2025-06-05 20:55:06 -0600

    Enhance ManageProductViewModel with form validation and product creation logic

--- feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductViewModel.kt
+++ feature/admin_panel/manage_product/src/commonMain/kotlin/dev/jdgarita/nutrisport/manage_product/ManageProductViewModel.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.jdgarita.nutrisport.data.domain.AdminRepository
+import dev.jdgarita.nutrisport.shared.domain.Product
 import dev.jdgarita.nutrisport.shared.domain.ProductCategory
+import kotlinx.coroutines.launch
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -16,7 +19,7 @@ data class ManageProductState(
     val description: String = "",
     val thumbnail: String = "",
     val category: ProductCategory = ProductCategory.Protein,
-    val flavors: List<String>? = null,
+    val flavors: String = "",
     val weight: Int? = null,
     val price: Double = 0.0
 )
@@ -26,6 +29,12 @@ class ManageProductViewModel(
 ) : ViewModel() {
     var screenState by mutableStateOf(ManageProductState())
         private set
+
+    val isFormValid: Boolean
+        get() = screenState.title.isNotEmpty() &&
+                screenState.description.isNotEmpty() &&
+                screenState.thumbnail.isNotEmpty() &&
+                screenState.price != 0.0
 
     fun updateId(value: String) {
         screenState = screenState.copy(id = value)
@@ -48,7 +57,7 @@ class ManageProductViewModel(
         screenState = screenState.copy(category = value)
     }
 
-    fun updateFlavors(value: List<String>) {
+    fun updateFlavors(value: String) {
         screenState = screenState.copy(flavors = value)
     }
 
@@ -58,5 +67,28 @@ class ManageProductViewModel(
 
     fun updatePrice(value: Double) {
         screenState = screenState.copy(price = value)
+    }
+
+    fun createNewProduct(
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+        viewModelScope.launch {
+            adminRepository.createNewProduct(
+                product = Product(
+                    id = screenState.id,
+                    title = screenState.title,
+                    description = screenState.description,
+                    thumbnail = screenState.thumbnail,
+                    category = screenState.category.name,
+                    flavors = screenState.flavors.split(","),
+                    weight = screenState.weight,
+                    price = screenState.price,
+                ),
+                onSuccess = onSuccess,
+                onError = onError
+            )
+
+        }
     }
 }
