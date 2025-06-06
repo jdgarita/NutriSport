42648cc622faedfc3dd07f71ccd6a48afb15674f
JD <juandiegogarita@gmail.com>
2025-06-05 20:01:49 -0600

    Implement photo picker functionality using ActivityResult API in Android

--- feature/admin_panel/manage_product/src/androidMain/kotlin/dev/jdgarita/nutrisport/manage_product/util/PhotoPicker.android.kt
+++ feature/admin_panel/manage_product/src/androidMain/kotlin/dev/jdgarita/nutrisport/manage_product/util/PhotoPicker.android.kt
@@ -1,14 +1,44 @@
 package dev.jdgarita.nutrisport.manage_product.util
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import dev.gitlive.firebase.storage.File
 
 actual class PhotoPicker {
 
+    private var openPhotoPicker by mutableStateOf(false)
+
     @Composable
     actual fun InitializePhotoPicker(onImageSelect: (File?) -> Unit) {
+        val openPhotoPickerState = remember { openPhotoPicker }
+        val pickMedia = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.PickVisualMedia(),
+        ) { uri ->
+            uri?.let {
+                onImageSelect(File(uri))
+            } ?: onImageSelect(null)
+            openPhotoPicker = false
+        }
+
+        LaunchedEffect(openPhotoPickerState) {
+            if (openPhotoPickerState) {
+                pickMedia.launch(
+                    PickVisualMediaRequest(
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
+        }
     }
 
     actual fun open() {
+        openPhotoPicker = true
     }
 }
