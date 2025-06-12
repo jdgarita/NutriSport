212d7781118ee401f5e71ff82ec210506112efdd
JD <juandiegogarita@gmail.com>
2025-06-11 21:04:00 -0600

    Add ProductsOverviewScreen and ViewModel for product overview functionality

---
+++ feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewScreen.kt
@@ -0,0 +1,2 @@
+package dev.jdgarita.nutrisport.products_overview
+
---
+++ feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewViewModel.kt
@@ -0,0 +1,6 @@
+package dev.jdgarita.nutrisport.products_overview
+
+import androidx.lifecycle.ViewModel
+
+class ProductsOverviewViewModel: ViewModel() {
+}
