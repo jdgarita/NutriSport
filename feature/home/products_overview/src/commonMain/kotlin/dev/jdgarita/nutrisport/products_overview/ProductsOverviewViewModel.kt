1b8dca6b5b34d4f52f23a87e90eda94e253decb3
JD <juandiegogarita@gmail.com>
2025-06-11 21:52:19 -0600

    Enhance ProductsOverviewViewModel to include product filtering and state management

--- feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewViewModel.kt
+++ feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewViewModel.kt
@@ -1,6 +1,20 @@
 package dev.jdgarita.nutrisport.products_overview
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.jdgarita.nutrisport.data.domain.ProductRepository
+import dev.jdgarita.nutrisport.shared.util.RequestState
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 
-class ProductsOverviewViewModel: ViewModel() {
+class ProductsOverviewViewModel(
+    private val productRepository: ProductRepository
+) : ViewModel() {
+    val products = productRepository.readNewAndDiscountedProducts()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = RequestState.Loading
+
+        )
 }
