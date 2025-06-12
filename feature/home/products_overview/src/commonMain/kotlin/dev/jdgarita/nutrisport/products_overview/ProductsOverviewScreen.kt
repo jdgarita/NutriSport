94069ddacbae15f17cb5ec34aff128665c08c612
JD <juandiegogarita@gmail.com>
2025-06-11 21:12:22 -0600

    Add Composable function for ProductsOverviewScreen with Koin ViewModel integration

--- feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewScreen.kt
+++ feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewScreen.kt
@@ -1,2 +1,9 @@
 package dev.jdgarita.nutrisport.products_overview
 
+import androidx.compose.runtime.Composable
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun ProductsOverviewScreen() {
+    val viewModel = koinViewModel<ProductsOverviewViewModel>()
+}
