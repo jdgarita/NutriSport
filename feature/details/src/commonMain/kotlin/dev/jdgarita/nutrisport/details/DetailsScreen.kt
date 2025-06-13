17726bc76aca688d7c27ff26f969e624d2a70e06
JD <juandiegogarita@gmail.com>
2025-06-12 21:21:41 -0600

    Refactor DetailsScreen to utilize product state and enhance loading/error handling

--- feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsScreen.kt
+++ feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsScreen.kt
@@ -1,11 +1,19 @@
 package dev.jdgarita.nutrisport.details
 
 import ContentWithMessageBar
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,15 +23,32 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import dev.jdgarita.nutrisport.shared.BebasNeueFont
+import dev.jdgarita.nutrisport.shared.BorderIdle
 import dev.jdgarita.nutrisport.shared.FontSize
 import dev.jdgarita.nutrisport.shared.IconPrimary
 import dev.jdgarita.nutrisport.shared.Resources
 import dev.jdgarita.nutrisport.shared.Surface
 import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.TextSecondary
+import dev.jdgarita.nutrisport.shared.component.InfoCard
+import dev.jdgarita.nutrisport.shared.component.LoadingCard
+import dev.jdgarita.nutrisport.shared.domain.ProductCategory
+import dev.jdgarita.nutrisport.shared.util.DisplayResult
 import org.jetbrains.compose.resources.painterResource
+import org.koin.compose.viewmodel.koinViewModel
 import rememberMessageBarState
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,7 +56,11 @@ import rememberMessageBarState
 fun DetailsScreen(
     navigateBack: () -> Unit
 ) {
+    val viewModel = koinViewModel<DetailsViewModel>()
     val messageBarState = rememberMessageBarState()
+
+    val product by viewModel.product.collectAsState()
+
     Scaffold(
         containerColor = Surface,
         topBar = {
@@ -66,80 +95,96 @@ fun DetailsScreen(
         }
     )
     { padding ->
-        ContentWithMessageBar(
-            modifier = Modifier
-                .padding(
-                    top = padding.calculateTopPadding(),
-                    bottom = padding.calculateBottomPadding()
-                ),
-            messageBarState = messageBarState,
-            errorMaxLines = 2,
-            contentBackgroundColor = Surface
-        ) {
-            Column {
-                Column(
+        product.DisplayResult(
+            onLoading = {
+                LoadingCard(
+                    modifier = Modifier.fillMaxSize()
+                )
+            },
+            onError = { errorMessage ->
+                InfoCard(
+                    image = Resources.Image.Cat,
+                    title = "Oops",
+                    subtitle = errorMessage
+                )
+            },
+            onSuccess = { selectedProduct ->
+                ContentWithMessageBar(
                     modifier = Modifier
-                        .weight(1f)
-                        .verticalScroll(rememberScrollState())
-                        .padding(horizontal = 24.dp)
-                        .padding(top = 12.dp)
+                        .padding(
+                            top = padding.calculateTopPadding(),
+                            bottom = padding.calculateBottomPadding()
+                        ),
+                    messageBarState = messageBarState,
+                    errorMaxLines = 2,
+                    contentBackgroundColor = Surface
                 ) {
-//                    AsyncImage(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .height(300.dp)
-//                            .clip(RoundedCornerShape(size = 12.dp))
-//                            .border(
-//                                width = 1.dp,
-//                                color = BorderIdle,
-//                                shape = RoundedCornerShape(size = 12.dp)
-//                            ),
-//                        model = ImageRequest.Builder(LocalPlatformContext.current)
-//                            .data(product.thumbnail)
-//                            .crossfade(enable = true)
-//                            .build(),
-//                        contentDescription = "Product thumbnail",
-//                        contentScale = ContentScale.Crop
-//                    )
-                    Spacer(modifier = Modifier.height(12.dp))
-//                    Row(
-//                        modifier = Modifier.fillMaxWidth(),
-//                        verticalAlignment = Alignment.CenterVertically,
-//                        horizontalArrangement = Arrangement.SpaceBetween
-//                    ) {
-//                        AnimatedContent(
-//                            targetState = product.category,
-//                        ) { category ->
-//                            if (ProductCategory.valueOf(category) == ProductCategory.Accessories) {
-//                                Spacer(modifier = Modifier.weight(1f))
-//                            } else {
-//                                Row(
-//                                    verticalAlignment = Alignment.CenterVertically
-//                                ) {
-//                                    Icon(
-//                                        modifier = Modifier.size(14.dp),
-//                                        painter = painterResource(Resources.Icon.Weight),
-//                                        contentDescription = "Weight icon"
-//                                    )
-//                                    Spacer(modifier = Modifier.height(4.dp))
-//                                    Text(
-//                                        text = "${product.weight}g",
-//                                        fontSize = FontSize.EXTRA_SMALL,
-//                                        color = TextPrimary
-//                                    )
-//                                }
-//                            }
-//                        }
-//                        Text(
-//                            text = "$${product.price}",
-//                            fontSize = FontSize.EXTRA_REGULAR,
-//                            color = TextSecondary,
-//                            fontWeight = FontWeight.Medium
-//                        )
-//                    }
+                    Column {
+                        Column(
+                            modifier = Modifier
+                                .weight(1f)
+                                .verticalScroll(rememberScrollState())
+                                .padding(horizontal = 24.dp)
+                                .padding(top = 12.dp)
+                        ) {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(300.dp)
+                                    .clip(RoundedCornerShape(size = 12.dp))
+                                    .border(
+                                        width = 1.dp,
+                                        color = BorderIdle,
+                                        shape = RoundedCornerShape(size = 12.dp)
+                                    ),
+                                model = ImageRequest.Builder(LocalPlatformContext.current)
+                                    .data(selectedProduct.thumbnail)
+                                    .crossfade(enable = true)
+                                    .build(),
+                                contentDescription = "Product thumbnail",
+                                contentScale = ContentScale.Crop
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                AnimatedContent(
+                                    targetState = selectedProduct.category,
+                                ) { category ->
+                                    if (ProductCategory.valueOf(category) == ProductCategory.Accessories) {
+                                        Spacer(modifier = Modifier.weight(1f))
+                                    } else {
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically
+                                        ) {
+                                            Icon(
+                                                modifier = Modifier.size(14.dp),
+                                                painter = painterResource(Resources.Icon.Weight),
+                                                contentDescription = "Weight icon"
+                                            )
+                                            Spacer(modifier = Modifier.height(4.dp))
+                                            Text(
+                                                text = "${selectedProduct.weight}g",
+                                                fontSize = FontSize.EXTRA_SMALL,
+                                                color = TextPrimary
+                                            )
+                                        }
+                                    }
+                                }
+                                Text(
+                                    text = "$${selectedProduct.price}",
+                                    fontSize = FontSize.EXTRA_REGULAR,
+                                    color = TextSecondary,
+                                    fontWeight = FontWeight.Medium
+                                )
+                            }
+                        }
+                        Column { }
+                    }
                 }
-                Column { }
             }
-        }
+        )
     }
 }
