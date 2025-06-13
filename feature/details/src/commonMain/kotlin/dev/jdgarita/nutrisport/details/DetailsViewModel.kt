7aa704d5b68e7255296ac52473bde94eb743e68f
JD <juandiegogarita@gmail.com>
2025-06-12 21:21:32 -0600

    Refactor DetailsViewModel to use SavedStateHandle and integrate product fetching logic

--- feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsViewModel.kt
+++ feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsViewModel.kt
@@ -1,7 +1,23 @@
 package dev.jdgarita.nutrisport.details
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.jdgarita.nutrisport.data.domain.ProductRepository
+import dev.jdgarita.nutrisport.shared.util.RequestState
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 
-class DetailsViewModel : ViewModel() {
+class DetailsViewModel(
+    private val productRepository: ProductRepository,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
+    val product =
+        productRepository.readProductByIdFlow(id = savedStateHandle.get<String>("id") ?: "")
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(),
+                initialValue = RequestState.Loading
+            )
 }
