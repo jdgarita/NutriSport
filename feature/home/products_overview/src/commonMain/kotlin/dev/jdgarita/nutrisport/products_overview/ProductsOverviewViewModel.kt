f8c4b8f29812a8d51b86265a4e21cd88f38785a3
JD <juandiegogarita@gmail.com>
2025-06-12 20:12:42 -0600

    Add newProducts state and combine with discountedProducts in ProductsOverviewViewModel

--- feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewViewModel.kt
+++ feature/home/products_overview/src/commonMain/kotlin/dev/jdgarita/nutrisport/products_overview/ProductsOverviewViewModel.kt
@@ -5,16 +5,44 @@ import androidx.lifecycle.viewModelScope
 import dev.jdgarita.nutrisport.data.domain.ProductRepository
 import dev.jdgarita.nutrisport.shared.util.RequestState
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
 class ProductsOverviewViewModel(
     private val productRepository: ProductRepository
 ) : ViewModel() {
-    val products = productRepository.readDiscountedProducts()
+    val discountedProducts = productRepository.readDiscountedProducts()
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = RequestState.Loading
-
         )
+
+    val newProducts = productRepository.readNewProducts()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = RequestState.Loading
+        )
+
+    val products = combine(
+        newProducts,
+        discountedProducts
+    ) { new, discounted ->
+        when {
+            new is RequestState.Success && discounted is RequestState.Success -> {
+                RequestState.Success(
+                    new.getSuccessData() + discounted.getSuccessData()
+                )
+            }
+
+            new.isError() -> new
+            discounted.isError() -> discounted
+            else -> RequestState.Loading
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = RequestState.Loading
+    )
 }
