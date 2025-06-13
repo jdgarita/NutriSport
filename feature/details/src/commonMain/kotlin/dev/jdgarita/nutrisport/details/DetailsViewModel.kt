4579885a6c58e346024934fdb78aa398c4fbbf4a
JD <juandiegogarita@gmail.com>
2025-06-12 23:21:50 -0600

    Add item selection and cart addition functionality to DetailsViewModel

--- feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsViewModel.kt
+++ feature/details/src/commonMain/kotlin/dev/jdgarita/nutrisport/details/DetailsViewModel.kt
@@ -1,16 +1,22 @@
 package dev.jdgarita.nutrisport.details
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.jdgarita.nutrisport.data.domain.CustomerRepository
 import dev.jdgarita.nutrisport.data.domain.ProductRepository
+import dev.jdgarita.nutrisport.shared.domain.CartItem
 import dev.jdgarita.nutrisport.shared.util.RequestState
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailsViewModel(
     private val productRepository: ProductRepository,
+    private val customerRepository: CustomerRepository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -22,10 +28,40 @@ class DetailsViewModel(
                 initialValue = RequestState.Loading
             )
 
-    var quantity = mutableStateOf(1)
+    var quantity by mutableStateOf(1)
+        private set
+
+    var selectedFlavor: String? by mutableStateOf(null)
         private set
 
     fun updateQuantity(newValue: Int) {
-        quantity.value = newValue
+        quantity = newValue
+    }
+
+    fun updateSelectedFlavor(newValue: String?) {
+        selectedFlavor = newValue
+    }
+
+    fun addItemToCard(
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+        viewModelScope.launch {
+            val productId = savedStateHandle.get<String>("id")
+
+            if (productId != null) {
+                customerRepository.addItemToCart(
+                    cartItem = CartItem(
+                        productId = productId,
+                        flavor = selectedFlavor,
+                        quantity = quantity
+                    ),
+                    onSuccess = onSuccess,
+                    onError = onError
+                )
+            } else {
+                onError("Product id is not found.")
+            }
+        }
     }
 }
