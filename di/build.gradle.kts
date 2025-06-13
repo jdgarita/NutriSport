ee1052ba366d20e3d0355e5322addd444bd533ea
JD <juandiegogarita@gmail.com>
2025-06-12 20:54:36 -0600

    Add details feature module to build configuration

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(project(path = ":feature:home"))
             implementation(project(path = ":feature:home:products_overview"))
             implementation(project(path = ":feature:profile"))
+            implementation(project(path = ":feature:details"))
             implementation(project(path = ":feature:admin_panel"))
             implementation(project(path = ":feature:admin_panel:manage_product"))
             implementation(project(path = ":data"))
