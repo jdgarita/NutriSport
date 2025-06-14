7594a8c451945da7ae08e2b8ec46f8dd72b833bc
JD <juandiegogarita@gmail.com>
2025-06-13 18:26:28 -0600

    Add CartScreen and CartViewModel for cart functionality

---
+++ feature/home/cart/src/commonMain/kotlin/dev/jdgarita/nutrisport/cart/CartScreen.kt
@@ -0,0 +1,7 @@
+package dev.jdgarita.nutrisport.cart
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CartScreen() {
+}
---
+++ feature/home/cart/src/commonMain/kotlin/dev/jdgarita/nutrisport/cart/CartViewModel.kt
@@ -0,0 +1,5 @@
+package dev.jdgarita.nutrisport.cart
+
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel()
