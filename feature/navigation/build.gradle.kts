b4e262a34c9b25de67c81ab49cde42e311bb8f92
JD <juandiegogarita@gmail.com>
2025-06-13 22:24:45 -0600

    Add checkout module dependency in build.gradle.kts

--- feature/navigation/build.gradle.kts
+++ feature/navigation/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(project(path = ":feature:profile"))
             implementation(project(path = ":feature:admin_panel"))
             implementation(project(path = ":feature:home:categories:category_search"))
+            implementation(project(path = ":feature:home:cart:checkout"))
             implementation(project(path = ":feature:details"))
             implementation(project(path = ":feature:admin_panel:manage_product"))
             implementation(project(path = ":shared"))
