053207e1a5a1ad6b9b4ef25459f6983a01c0df45
JD <juandiegogarita@gmail.com>
2025-06-14 13:39:15 -0600

    Add PayPal API endpoints to Constants for authentication and checkout

--- shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/Constants.kt
+++ shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val MAX_QUANTITY = 10
     const val MIN_QUANTITY = 1
 
+    const val PAYPAL_AUTH_ENDPOINT = "https://api-m.sandbox.paypal.com/v1/oauth2/token"
+    const val PAYPAL_CHECKOUT_ENDPOINT = "https://api-m.sandbox.paypal.com/v2/checkout/orders"
+
     const val RETURN_URL = "com.stevdza.san.nutrisport://paypalpay?success=true"
     const val CANCEL_URL = "com.stevdza.san.nutrisport://paypalpay?cancel=true"
 }
