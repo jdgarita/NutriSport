6d7ed04f203ec7aba71f64d6742c658dd832bff5
JD <juandiegogarita@gmail.com>
2025-06-13 18:50:25 -0600

    Add message bar and loading state to CartScreen for improved user feedback

--- feature/home/cart/src/commonMain/kotlin/dev/jdgarita/nutrisport/cart/CartScreen.kt
+++ feature/home/cart/src/commonMain/kotlin/dev/jdgarita/nutrisport/cart/CartScreen.kt
@@ -1,7 +1,107 @@
 package dev.jdgarita.nutrisport.cart
 
+import ContentWithMessageBar
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.jdgarita.nutrisport.cart.component.CartItemCard
+import dev.jdgarita.nutrisport.shared.Resources
+import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.SurfaceBrand
+import dev.jdgarita.nutrisport.shared.SurfaceError
+import dev.jdgarita.nutrisport.shared.TextPrimary
+import dev.jdgarita.nutrisport.shared.TextWhite
+import dev.jdgarita.nutrisport.shared.component.InfoCard
+import dev.jdgarita.nutrisport.shared.component.LoadingCard
+import dev.jdgarita.nutrisport.shared.util.DisplayResult
+import dev.jdgarita.nutrisport.shared.util.RequestState
+import org.koin.compose.viewmodel.koinViewModel
+import rememberMessageBarState
 
 @Composable
 fun CartScreen() {
+    val messageBarState = rememberMessageBarState()
+    val viewModel = koinViewModel<CartViewModel>()
+    val cartItemsWithProducts by viewModel.cartItemsWithProducts.collectAsState(RequestState.Loading)
+
+    ContentWithMessageBar(
+        contentBackgroundColor = Surface,
+        messageBarState = messageBarState,
+        errorMaxLines = 2,
+        errorContainerColor = SurfaceError,
+        errorContentColor = TextWhite,
+        successContainerColor = SurfaceBrand,
+        successContentColor = TextPrimary
+    ) {
+        cartItemsWithProducts.DisplayResult(
+            onLoading = { LoadingCard(modifier = Modifier.fillMaxSize()) },
+            onSuccess = { data ->
+                if (data.isNotEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(12.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        items(
+                            items = data,
+                            key = { data.hashCode().toString() }
+                        ) { pair ->
+                            CartItemCard(
+                                cartItem = pair.first,
+                                product = pair.second,
+                                onMinusClick = { quantity ->
+//                                    viewModel.updateCartItemQuantity(
+//                                        id = pair.first.id,
+//                                        quantity = quantity,
+//                                        onSuccess = {},
+//                                        onError = { messageBarState.addError(it) }
+//                                    )
+                                },
+                                onPlusClick = { quantity ->
+//                                    viewModel.updateCartItemQuantity(
+//                                        id = pair.first.id,
+//                                        quantity = quantity,
+//                                        onSuccess = {},
+//                                        onError = { messageBarState.addError(it) }
+//                                    )
+                                },
+                                onDeleteClick = {
+//                                    viewModel.deleteCartItem(
+//                                        id = pair.first.id,
+//                                        onSuccess = {},
+//                                        onError = { messageBarState.addError(it) }
+//                                    )
+                                }
+                            )
+                        }
+                    }
+                } else {
+                    InfoCard(
+                        image = Resources.Image.ShoppingCart,
+                        title = "Empty Cart",
+                        subtitle = "Check some of our products."
+                    )
+                }
+            },
+            onError = { message ->
+                InfoCard(
+                    image = Resources.Image.Cat,
+                    title = "Oops!",
+                    subtitle = message
+                )
+            },
+            transitionSpec = fadeIn() togetherWith fadeOut()
+        )
+    }
 }
