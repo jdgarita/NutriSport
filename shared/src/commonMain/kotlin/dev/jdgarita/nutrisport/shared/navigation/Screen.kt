66d5c485d1e98ee3910a97cc1fabb74a788c9d1c
JD <juandiegogarita@gmail.com>
2025-06-13 20:10:16 -0600

    Add CategorySearch screen for handling category-specific searches

--- shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/navigation/Screen.kt
+++ shared/src/commonMain/kotlin/dev/jdgarita/nutrisport/shared/navigation/Screen.kt
@@ -21,6 +21,11 @@ sealed class Screen {
     data object Categories : Screen()
 
     @Serializable
+    data class CategorySearch(
+        val category: String
+    ) : Screen()
+
+    @Serializable
     data object Profile : Screen()
 
     @Serializable
