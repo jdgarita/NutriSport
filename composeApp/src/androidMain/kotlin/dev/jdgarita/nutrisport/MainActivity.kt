1013abfec5a40a4fd687ffe237f365e7e4b6a4e4
JD <juandiegogarita@gmail.com>
2025-06-14 15:01:04 -0600

    Add onNewIntent method to handle PayPal payment results in MainActivity

--- composeApp/src/androidMain/kotlin/dev/jdgarita/nutrisport/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/jdgarita/nutrisport/MainActivity.kt
@@ -1,13 +1,12 @@
 package dev.jdgarita.nutrisport
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +28,25 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val uri = intent.data
+
+        val isSuccess = uri?.getQueryParameter("success")
+        val isCancelled = uri?.getQueryParameter("cancel")
+        val token = uri?.getQueryParameter("token")
+    }
+//    PreferencesRepository.savePayPalData(
+//        isSuccess = isSuccess?.toBooleanStrictOrNull(),
+//        error = if (isCancelled == "null") null
+//        else "Payment has been canceled.",
+//        token = token
+//    )
+//        intentHandler.navigateToPaymentCompleted(
+//            isSuccess = isSuccess?.toBooleanStrictOrNull(),
+//            error = if (isCancelled == "null") null
+//            else "Payment has been canceled.",
+//            token = token
+//        )
 }
