f8fe5409607780c137f4b846b7cb7e22072461d0
JD <juandiegogarita@gmail.com>
2025-06-13 19:14:34 -0600

    Add updateCartItemQuantity method to CustomerRepository for modifying cart item quantities

--- data/src/commonMain/kotlin/dev/jdgarita/nutrisport/data/domain/CustomerRepository.kt
+++ data/src/commonMain/kotlin/dev/jdgarita/nutrisport/data/domain/CustomerRepository.kt
@@ -31,4 +31,11 @@ interface CustomerRepository {
         onSuccess: () -> Unit,
         onError: (String) -> Unit
     )
+
+    suspend fun updateCartItemQuantity(
+        id: String,
+        quantity: Int,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    )
 }
