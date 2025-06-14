e901801a5e22386ad3da0feec006d0e1bd9c6daf
JD <juandiegogarita@gmail.com>
2025-06-14 10:19:41 -0600

    Implement PaymentCompleted screen logic with loading, success, and error states

--- feature/payment_completed/src/commonMain/kotlin/dev/jdgarita/nutrisport/payment_completed/PaymentCompleted.kt
+++ feature/payment_completed/src/commonMain/kotlin/dev/jdgarita/nutrisport/payment_completed/PaymentCompleted.kt
@@ -1,9 +1,76 @@
 package dev.jdgarita.nutrisport.payment_completed
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.jdgarita.nutrisport.shared.Resources
+import dev.jdgarita.nutrisport.shared.Surface
+import dev.jdgarita.nutrisport.shared.component.InfoCard
+import dev.jdgarita.nutrisport.shared.component.LoadingCard
+import dev.jdgarita.nutrisport.shared.component.PrimaryButton
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun PaymentCompleted(
-    navigateBack: () -> Unit
+    navigateBack: () -> Unit,
 ) {
+    val viewModel = koinViewModel<PaymentViewModel>()
+    val screenState = viewModel.screenState
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Surface)
+            .systemBarsPadding()
+            .padding(all = 24.dp)
+    ) {
+        screenState.DisplayResult(
+            onLoading = { LoadingCard(modifier = Modifier.fillMaxSize()) },
+            onSuccess = {
+                Column {
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        InfoCard(
+                            title = "Success!",
+                            subtitle = "Your purchase is on the way.",
+                            image = Resources.Image.Checkmark
+                        )
+                    }
+                    PrimaryButton(
+                        text = "Go back",
+                        icon = Resources.Icon.RightArrow,
+                        onClick = navigateBack
+                    )
+                }
+            },
+            onError = { message ->
+                Column {
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        InfoCard(
+                            title = "Oops!",
+                            subtitle = message,
+                            image = Resources.Image.Cat
+                        )
+                    }
+                    PrimaryButton(
+                        text = "Go back",
+                        icon = Resources.Icon.RightArrow,
+                        onClick = navigateBack
+                    )
+                }
+            }
+        )
+    }
 }
